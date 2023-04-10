9f8a05fe2b855ec0ce430901b19f0d8b6b7b30c0
vinyatu <107617394+vinyatu@users.noreply.github.com>
2023-04-10 15:02:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import anderson_ksamp
 
 chat_id = 5991293770
 
 def solution(x: np.array, y: np.array) -> bool:
-    _, pvalue = ks_2samp(x, y)
-    return pvalue < 0.07
+    res = anderson_ksamp([x, y])
+    return res.pvalue < 0.07
